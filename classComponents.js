20f6ecc8cf5861d8452529e7895f62a531bbde72
msultan27 <rizwangithub84@gmail.com>
2023-02-22 23:52:32 +0500

    add classBasedComoponents and update class components

---
+++ classComponents.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class classComponents extends React.Component {
+
+
+	render() {
+		return (
+			<div>
+                
+                <h1>Rizwan Shaikh</h1>
+                 
+			</div>
+		);
+	}
+}
+
+
